c3239cbf01978dc2c68aa75edef4eb2559fde8be
memorandumtk <memorandum876@outlook.com>
2024-06-16 01:01:46 -0400

    2024/6/15: ホーム画面にカルーセル機能を追加し、画像が表示されるように。(初期実装)

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,6 @@
 import fetchData from "./utils/fetchJson.js";
 
-const cardsContainer = document.getElementById('cards-container');
+const carouselItems = document.querySelectorAll('.carousel-item');
 
 /**
  * 植物サンプルデータをセッションストレージに保存し、データを返す関数。
@@ -8,7 +8,6 @@ const cardsContainer = document.getElementById('cards-container');
 async function storeSamplePlantsData() {
 
     const plantsData = await fetchData('/data/PlantsSample.json');
-    console.log(plantsData);
 
     sessionStorage.setItem('plants', JSON.stringify(plantsData));
 
@@ -16,86 +15,30 @@ async function storeSamplePlantsData() {
 }
 
 /**
- * 入力されたデータからカード（Bootstrap）を作成する関数。
+ * カルーセルアイテムに植物データを表示する関数。
  */
-function createCard(plantData) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+function displayPlantsOnCarousel(plants) {
+    carouselItems.forEach((item, index) => {
+        if (index < 3) {
+            const plant = plants[index];
+            const img = item.querySelector('img');
 
-    const cardImg = document.createElement('img');
-    cardImg.classList.add('card-img-top');
-    cardImg.src = plantData.image_url;
-    cardImg.alt = plantData.name;
-
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-
-    const cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = plantData.japanese_name;
-
-    const cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = plantData.description;
-
-    const cardButton = document.createElement('a');
-    cardButton.classList.add('btn', 'btn-primary');
-    cardButton.href = '#';
-    cardButton.textContent = 'Detail';
-
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(cardButton);
-
-    card.appendChild(cardImg);
-    card.appendChild(cardBody);
-
-    return card;
+            img.src = plant.image_url;
+        }
+    });
 }
 
 /**
- * colをクラスに持つdiv要素を作成する関数。
+ * ページが読み込まれたときに実行される関数。
  */
-function createColDiv() {
-    const colDiv = document.createElement('div');
-    colDiv.classList.add('col');
-
-    return colDiv;
-}
-
-/**
- * rowをクラスに持つdiv要素を作成する関数。
- */
-function createRowDiv() {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-
-    return rowDiv;
-}
-
-
 window.onload = async () => {
 
     // 植物サンプルデータをセッションストレージに保存する関数を呼出し、データを保持。
     const plantsData = await storeSamplePlantsData();
 
-    console.log(plantsData);
-    // 植物データからカードを作成。
-    let rowDiv;
-    for (let i = 0; i < plantsData.length; i++) {
+    const sortedPlants = plantsData.sort((a, b) => b.number_of_liked - a.number_of_liked);
 
-        // 1行ごとに3つのカードが表示されるようにする。
-        if (i % 3 === 0) {
-            rowDiv = createRowDiv();
-        }
+    console.log(sortedPlants);
 
-        const card = createCard(plantsData[i]);
-        const colDiv = createColDiv();
-        colDiv.appendChild(card);
-
-        rowDiv.appendChild(colDiv);
-
-        cardsContainer.appendChild(rowDiv);
-    }
-
+    displayPlantsOnCarousel(sortedPlants);
 }
